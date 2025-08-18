8718d31a57c1df8d2a21f045355e68eff494e822
omar93939 <om.markovinovic@gmail.com>
2025-08-18 03:27:45 +0200

    Testing error

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -39,7 +39,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
         thumbnailImg.alt = media.Title;
         thumbnailImg.onerror = () => {
-          delete thumbnailImg.dataset.fancybox;
+          thumbnail.href = '/images/Processing.png';
           thumbnailImg.onerror = null;
           thumbnailImg.src = '/images/Processing.png';
           thumbnailImg.dataset.preview = '/images/Processing.png';
